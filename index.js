71e7ec451f3f626374b54267480c43f5920dab3b
sunnytyagi1992 <sunnyinfopedia@gmail.com>
2024-11-18 16:25:56 +0530

    mj

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require("express");
+import express from 'express';
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (res) => res.send("Express on Vercel"));
 
 app.listen(3005, () => console.log("Server ready on port 3000."));
 
